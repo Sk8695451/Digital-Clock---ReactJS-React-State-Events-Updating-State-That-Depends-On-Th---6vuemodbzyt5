0c7aa380b1dd102954d3ce23bff84e884e295873
unknown <sk8695451>
2022-11-23 22:34:18 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,47 @@
-import React from 'react'
-import '../styles/App.css';
-const App = () => {
+import React, { Fragment } from 'react'
+// import '../styles/App.css';
+// const App = () => {
 
-  return (
-    <div id="main">
-      <div className="date-time"></div>
-    </div>
-  )
+//   return (
+//     <div id="main">
+//       <div className="date-time"></div>
+//     </div>
+//   )
+// }
+
+
+// export default App;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div id="main">
+        <div className="date-time">
+          <h2>It is {this.state.date.toLocaleString()}.</h2>
+        </div>
+      </div>
+    );
+  }
 }
-
-
 export default App;
